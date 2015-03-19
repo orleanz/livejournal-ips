c94b44c8c005b53c2cd7faf0c73b535a2d9ab748
orleanz <undead.electrical.zombie@gmail.com>
2015-03-19 10:24:18 +0100

    added caching, relaxed span selection criterium

--- content.js
+++ content.js
@@ -1,28 +1,48 @@
-(function doReplace() {
+(function() {
 
-    $("span.b-leaf-ipaddr").each(function(ind, span) {
+    // var selector = "span.b-leaf-ipaddr";
+    var selector = "span";
+
+    var collectedIPs = {};
+
+    $(selector).each(function(ind, span) {
         var re = /\d+\.\d+\.\d+\.\d+/i;
         var found = span.textContent.match(re);
         if (found) {
-
             span.textContent = span.textContent + " address search in progress ... ";
-
             var ip = found[0];
+            if (!collectedIPs[ip]) {
+                collectedIPs[ip] = [];
+            }
+            collectedIPs[ip].push(span);
+        }
+    });
+
+    for (ip in collectedIPs) {
+
+        (function(ip) {
+
+            var spans = collectedIPs[ip];
 
             $.ajax({
                 type: "GET",
                 url: "http://lentascope.net/ip/" + ip,
-                success: function(msg){
-                    span.textContent = "(from " + ip + ", " + msg + ")";
+                success: function(geo_info){
+                    for (var i = 0; i < spans.length; i++) {
+                        spans[i].textContent = "(from " + ip + ", " + geo_info + ")";
+                    }
                 },
                 error: function(XMLHttpRequest, textStatus, errorThrown) {
-                    console.error(textStatus);
-                    span.textContent = "(from " + ip + "), " + textStatus + ":" + errorThrown;
+                    for (var i = 0; i < spans.length; i++) {
+                        spans[i].textContent = "(from " + ip + "), " + textStatus + ":" + errorThrown;
+                    }
                 }
             });
-        }
-    });
 
+        })(ip);
+
+    }
+   
  }
 
 )();
